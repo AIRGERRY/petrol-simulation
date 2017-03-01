6a6024184ed79c34c4607ec400c67b8d71ef5394
Ollie <nyeo@aston.ac.uk>
2017-03-01 23:35:49 +0000

    Create Truck class

---
+++ src/aston/vehicle/Truck.java
@@ -0,0 +1,20 @@
+package aston.vehicle;
+
+/**
+ * 
+ * 
+ * @author Ollie
+ * @version 1.0
+ * @since 1 Mar 2017
+ *
+ */
+
+public class Truck extends Vehicle {
+	
+	/**
+	 * Defines a Truck according to the specification
+	 */
+	public Truck() {
+		super(2.0, 30, 40);
+	}
+}
