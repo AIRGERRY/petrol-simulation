ed43e000545aa3169e9d9e2805f2f82a3d596884
Ollie <nyeo@aston.ac.uk>
2017-03-04 15:08:24 +0000

    Create Queue class for handling customers

---
+++ src/aston/station/Queue.java
@@ -0,0 +1,18 @@
+package aston.station;
+
+import java.util.concurrent.ArrayBlockingQueue;
+
+/**
+ * 
+ * 
+ * @author Ollie
+ * @version 1.0
+ * @since 4 Mar 2017
+ *
+ */
+
+public class Queue {
+	//Needs a get method for classes to call
+	//Needs a put method for classes to call
+	
+}
