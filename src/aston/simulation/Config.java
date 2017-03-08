4b7251e2fedf9faca24038301826fe8927ecdfde
Ollie <nyeo@aston.ac.uk>
2017-03-08 00:40:38 +0000

    Add constants file for easy settings changes

---
+++ src/aston/simulation/Config.java
@@ -0,0 +1,64 @@
+package aston.simulation;
+
+/**
+ * Global project constants
+ * 
+ * @author Ollie
+ * @version 1.0
+ * @since 8 Mar 2017
+ *
+ */
+
+public class Config {
+	
+	public static final int TICKS_PER_MINUTE = 6;
+	
+	//Tank size
+	public static final int MOTORBIKE_TANK = 5;
+	public static final int SMALLCAR_TANK_LOW = 7;
+	public static final int SMALLCAR_TANK_HIGH = 9;
+	public static final int SEDAN_TANK_LOW = 12;
+	public static final int SEDAN_TANK_HIGH = 18;
+	public static final int TRUCK_TANK_LOW = 30;
+	public static final int TRUCK_TANK_HIGH = 40;
+	
+	//Happy time
+	public static final int MOTORBIKE_HAPPY_TIME = 0;
+	public static final int SMALLCAR_HAPPY_TIME = 5;
+	public static final int SEDAN_HAPPY_TIME = 10;
+	public static final int TRUCK_HAPPY_TIME = 8;
+	
+	//Shopping price range
+	public static final int SMALLCAR_SHOPPING_LOW = 5;
+	public static final int SMALLCAR_SHOPPING_HIGH = 10;
+	public static final int SEDAN_SHOPPING_LOW = 8;
+	public static final int SEDAN_SHOPPING_HIGH = 16;
+	public static final int TRUCK_SHOPPING_LOW = 15;
+	public static final int TRUCK_SHOPPING_HIGH = 20;
+	
+	//Shopping time range
+	public static final int SMALLCAR_SHOPPING_TIME_LOW = 2;
+	public static final int SMALLCAR_SHOPPING_TIME_HIGH = 4;
+	public static final int SEDAN_SHOPPING_TIME_LOW = 2;
+	public static final int SEDAN_SHOPPING_TIME_HIGH = 5;
+	public static final int TRUCK_SHOPPING_TIME_LOW = 4;
+	public static final int TRUCK_SHOPPING_TIME_HIGH = 6;
+	
+	//Shopping probability
+	public static final double SMALLCAR_SHOPPING = 0.3;
+	public static final double SEDAN_SHOPPING = 0.4;
+	public static final double TRUCK_SHOPPING = 1.0;
+	
+	//Vehicle probability
+	public static final double MOTORBIKE_PROBABILITY = 0.05; //0.01, 0.02, 0.03, 0.04, 0.05
+	public static final double SMALLCAR_PROBABILITY = 0.05;
+	public static final double SEDAN_PROBABILITY = 0.05;
+	public static double TRUCK_PROBABILITY = 0.02;
+	
+	//Servicer count
+	public static final int PUMP_COUNT = 4;
+	public static final int TILL_COUNT = 4;
+	
+	//Allow trucks
+	public static final boolean ALLOW_TRUCKS = true;
+}
