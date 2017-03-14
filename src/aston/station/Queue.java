f80d8ea7f0a5b91bdf0398b68532d539deff397c
Ollie <nyeo@aston.ac.uk>
2017-03-14 00:24:51 +0000

    Add put and take methods

--- src/aston/station/Queue.java
+++ src/aston/station/Queue.java
@@ -3,24 +3,46 @@ package aston.station;
 import java.util.concurrent.ArrayBlockingQueue;
 
 import aston.person.Customer;
+import aston.vehicle.Vehicle;
 
 /**
  * Queue for servicers
  * 
- * @author Ollie
- * @version 1.0
+ * @author Ollie, Sope
+ * @version 1.2
  * @since 4 Mar 2017
  *
  */
 
 public class Queue {
 
-	//Needs a get method for classes to call
-	//Needs a put method for classes to call
-	
-	//queue object
-	
-	//queue capacity
-	
-	//queue weight
+	private ArrayBlockingQueue<Vehicle> queue;
+
+	private double queueLevel;
+
+	public Queue() {
+		this.queue = new ArrayBlockingQueue<Vehicle>(4);
+	}
+
+	public Vehicle take() {
+		try {
+			Vehicle vehicle = this.queue.take();
+			this.queueLevel -= vehicle.getQueueSize();
+			return vehicle;
+		} catch (InterruptedException ex) {
+			System.out.println("Problem taking object from queue");
+			return null;
+		}
+	}
+
+	public void put(Vehicle vehicle) {
+		try {
+			this.queueLevel += vehicle.getQueueSize();
+			this.queue.put(vehicle);
+		} catch (InterruptedException ex) {
+			System.out.println("Problem adding object to queue - " + vehicle.toString());
+		}
+
+	}
+
 }
