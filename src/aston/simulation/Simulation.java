7c634e2c5ddc1170fba8b51a287f73f6be743a26
Ollie <nyeo@aston.ac.uk>
2017-03-14 13:14:33 +0000

    Test vehicle tank randomisation and toString methods

--- src/aston/simulation/Simulation.java
+++ src/aston/simulation/Simulation.java
@@ -1,6 +1,9 @@
 package aston.simulation;
 
 import aston.vehicle.*;
+
+import com.sun.org.apache.bcel.internal.generic.NEW;
+
 import aston.resources.*;
 import aston.station.*;
 
@@ -18,5 +21,26 @@ public class Simulation {
 	public static void main(String args[]) {
 		Station station = Station.getInstance();
 		System.out.println("Simulator initialised");
+		
+		System.out.println(new Motorbike().toString());
+		System.out.println(new Motorbike().toString());
+		System.out.println(new Motorbike().toString());
+		System.out.println(new Motorbike().toString());
+		
+		System.out.println(new Sedan().toString());
+		System.out.println(new Sedan().toString());
+		System.out.println(new Sedan().toString());
+		System.out.println(new Sedan().toString());
+		
+		System.out.println(new SmallCar().toString());
+		System.out.println(new SmallCar().toString());
+		System.out.println(new SmallCar().toString());
+		System.out.println(new SmallCar().toString());
+		
+		System.out.println(new Truck().toString());
+		System.out.println(new Truck().toString());
+		System.out.println(new Truck().toString());
+		System.out.println(new Truck().toString());
+		
 	}
 }
