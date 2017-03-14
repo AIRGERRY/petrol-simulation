4155a14ed0cd4c2a260e5c9cfa5c402175b621a5
Ollie <nyeo@aston.ac.uk>
2017-03-14 00:53:03 +0000

    Create ShoppingArea class, add tick method

---
+++ src/aston/station/ShoppingArea.java
@@ -0,0 +1,38 @@
+package aston.station;
+
+import aston.person.Customer;
+import java.util.ArrayList;
+import java.util.Iterator;
+
+/**
+ * ShoppingArea class for happy customers
+ * 
+ * @author Ollie
+ * @version 1.0
+ * @since 14 Mar 2017
+ *
+ */
+
+public class ShoppingArea {
+	
+	private ArrayList<Customer> customers;
+	
+	public ShoppingArea() {
+		this.customers = new ArrayList<Customer>();
+		
+	}
+	
+	public void tick() {
+		Iterator<Customer> customerIterator = customers.iterator();
+		while(customerIterator.hasNext()) {
+			Customer customer = customerIterator.next();
+			if (customer.getTime() == 0) {
+				Station.getInstance().joinTill(customer);
+				customerIterator.remove();
+			} else {
+				customer.decrementTime();
+			}
+		}
+	}
+	
+}
