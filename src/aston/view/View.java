754fb555862c52892f5978c71187a6bc9ca33507
Zak <Zak@10.76.6.16>
2017-03-14 13:45:25 +0000

    Add base methods

--- src/aston/view/View.java
+++ src/aston/view/View.java
@@ -11,4 +11,40 @@ package aston.view;
 
 public class View {
 	
+	private boolean atPump = true;
+	private double fuelCount = 0;
+	private boolean atTill = true;
+	private double custSpent = 0;
+	
+	public void vehicleAtPump(){
+		if(atPump){
+		System.out.println("A vehicle is at the pump");
+	} else {
+		System.out.println("A vehcile has left the pump");}
+	}
+	
+	public void atTill(){
+		if(atTill){
+		System.out.println("A customer is at the till");
+	} else {
+		System.out.println("A customer has left the till");
+		}
+	}
+	
+	public void amountSpent(){
+		if (custSpent > 0){
+		System.out.println("The customer has spent " + custSpent );
+	} else {
+		System.out.println("The customer has not spent anything");
+		}
+	}
+	
+	public void amountOfFuel(){
+		if (fuelCount > 0){
+		System.out.println("Vechicle topped up " + fuelCount + "gallons");
+	} else {
+		System.out.println("No fuel was used");
+		}
+	
+	}
 }
