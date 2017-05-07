ef8742d8d45a47f27c1927abd3aa34a090bbd538
Liam Acton <rio@100.71.1.235>
2017-05-07 23:56:16 +0100

    tests

--- tests/aston/person/CustomerTest.java
+++ tests/aston/person/CustomerTest.java
@@ -13,8 +13,18 @@ import static org.junit.Assert.*;
 import org.junit.Test;
 
 public class CustomerTest {
+	
+	/* Tests method setHappy*/
+	
+	@Test
+	  public void setHappy(){
+	      Customer customer = new Customer(true);
+	      customer.setHappy(true);
+	      assertTrue(customer.isHappy());
+	  }
 
-	/* Tests method isHappy when it equals true */
+	
+	/* Tests method isHappy */
 	
 	@Test
 	  public void isHappyTest1(){
@@ -23,13 +33,48 @@ public class CustomerTest {
 	      assertTrue(boolean0);
 	  }
 	
-	/* Tests method isHappy when it equals false */
-	
 	@Test
 	  public void isHappyTest2(){
-	      Customer customer0 = new Customer(false);
-	      boolean boolean0 = customer0.isHappy();
+	      Customer customer = new Customer(false);
+	      boolean boolean0 = customer.isHappy();
 	      assertFalse(boolean0);
+	  }
+	
+	/*Test Method MoneySpent */
+	
+	@Test
+	  public void MoneySpent1(){
+	      Customer customer = new Customer(false);
+	      customer.setMoneySpent(100);
+	      int int0 = customer.getMoneySpent();
+	      assertEquals(100, int0);
+	  }
+
+	  public void MoneySpent2(){
+	      Customer customer = new Customer(false);
+	      int int0 = customer.getMoneySpent();
+	      assertEquals(0, int0);
+	      assertFalse(customer.isHappy());
+	  }
+	
+	/*Test Method Set Time */
+	
+	 @Test
+	  public void setTime(){
+	      Customer customer = new Customer(true);
+	      customer.setTime(100);
+	      int int0 = customer.getTime();
+	      assertEquals(100, int0);
+	  }
+	 
+	 /* Tests method getTime */
+		
+	 @Test
+	  public void getTime(){
+	      Customer customer = new Customer(true);
+	      int int0 = customer.getTime();
+	      assertEquals(0, int0);
+	      assertTrue(customer.isHappy());
 	  }
 	
 	/* Tests method decrementTime, where the value of getTime is subtracted by 1. */
@@ -40,16 +85,6 @@ public class CustomerTest {
 	      customer.decrementTime();
 	      int int0 = customer.getTime();
 	      assertEquals((-1), int0);
-	  }
-	
-	/* Tests method getTime */
-	
-	 @Test
-	  public void getTime(){
-	      Customer customer = new Customer(true);
-	      int int0 = customer.getTime();
-	      assertEquals(0, int0);
-	      assertTrue(customer.isHappy());
 	  }
 	
 }
